5cfbc7bd8bba544540784b14f9cd02c038dd45f6
Jirayut Poksawat <jpoksawat@uh.edu>
2016-12-08 00:28:28 -0600

    add sample data

--- src/weather/urls.py
+++ src/weather/urls.py
@@ -6,8 +6,6 @@ app_name = 'weather'
 urlpatterns = [
     url(r'^push/$', views.push, name='push'),
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^temperature/(?P<sensor_id>[0-9]+)/$', views.temperature_detail, name='temperature_detail'),
-    url(r'^temperature/(?P<sensor_id>[0-9]+)/chart/$', views.temperature_chart, name='temperature_chart'),
-    url(r'^humidity/(?P<sensor_id>[0-9]+)/$', views.humidity_detail, name='humidity_detail'),
-    url(r'^humidity/(?P<sensor_id>[0-9]+)/chart/$', views.humidity_chart, name='humidity_chart'),
+    url(r'^(?P<sensor_id>[0-9]+)/(?P<field>\w+)/$', views.detail, name='detail'),
+    url(r'^(?P<sensor_id>[0-9]+)/(?P<field>\w+)/chart/$$', views.chart, name='chart'),
 ]
--- src/weather/views.py
+++ src/weather/views.py
@@ -21,22 +21,35 @@ class IndexView(generic.ListView):
         """
         return Sensor.objects.all()
 
-def temperature_detail(request, sensor_id):
+def detail(request, sensor_id, field):
     sensor = get_object_or_404(Sensor, pk=sensor_id)
-    sensor_list = sensor.temperature_set.all().order_by('-timestamp')[:100]
-    context = {'sensor_list': sensor_list}
+
+    if field == 'temperature':
+        sensor_list = sensor.temperature_set.all().order_by('-timestamp')[:100]
+    elif field == 'humidity':
+        sensor_list = sensor.humidity_set.all().order_by('-timestamp')[:100]
+
+    context = {
+        'sensor_name': sensor.name,
+        'field': field.capitalize(),
+        'sensor_list': sensor_list}
 
     return render(request, 'weather/table.html', context)
 
-def temperature_chart(request, sensor_id):
+def chart(request, sensor_id, field):
     sensor = get_object_or_404(Sensor, pk=sensor_id)
+
+    if field == 'temperature':
+        qs = sensor.temperature_set
+    elif field == 'humidity':
+        qs = sensor.humidity_set
 
     # Step 1: Create a DataPool with the data we want to retrieve.
     weatherdata = \
         DataPool(
             series=
                 [{'options': {
-                    'source': sensor.temperature_set
+                    'source': qs
                               .extra({'date': 'date(timestamp)'})
                               .values('date')
                               .annotate(avg=Avg('value'))
@@ -46,7 +59,7 @@ def temperature_chart(request, sensor_id):
                     'avg']},
 
                  {'options': {
-                     'source': sensor.temperature_set
+                     'source': qs
                                .extra({'date': 'date(timestamp)'})
                                .values('date')
                                .annotate(min=Min('value'))
@@ -56,7 +69,7 @@ def temperature_chart(request, sensor_id):
                      'min']},
 
                  {'options': {
-                     'source': sensor.temperature_set
+                     'source': qs
                                .extra({'date': 'date(timestamp)'})
                                .values('date')
                                .annotate(max=Max('value'))
@@ -80,80 +93,13 @@ def temperature_chart(request, sensor_id):
                 }}],
             chart_options =
                 {'title': {
-                    'text': 'Weather Data'},
+                    'text': sensor.name + ' - ' + field.capitalize()},
                  'xAxis': {
                     'title': {
                         'text': 'Date'}}})
 
     context = {'weatherchart': cht}
-    # Step 3: Send the chart object to the template.
-    return render(request, 'weather/chart.html', context)
 
-def humidity_detail(request, sensor_id):
-    sensor = get_object_or_404(Sensor, pk=sensor_id)
-    sensor_list = sensor.humidity_set.all().order_by('-timestamp')
-    context = {'sensor_list': sensor_list}
-
-    return render(request, 'weather/table.html', context)
-
-def humidity_chart(request, sensor_id):
-    sensor = get_object_or_404(Sensor, pk=sensor_id)
-
-    # Step 1: Create a DataPool with the data we want to retrieve.
-    weatherdata = \
-        DataPool(
-            series=
-                [{'options': {
-                    'source': sensor.humidity_set
-                              .extra({'date': 'date(timestamp)'})
-                              .values('date')
-                              .annotate(avg=Avg('value'))
-                              .order_by('-date')[:30],},
-                  'terms': [
-                    'date',
-                    'avg']},
-
-                 {'options': {
-                     'source': sensor.humidity_set
-                               .extra({'date': 'date(timestamp)'})
-                               .values('date')
-                               .annotate(min=Min('value'))
-                               .order_by('-date')[:30],},
-                   'terms': [
-                     {'date_min': 'date'},
-                     'min']},
-
-                 {'options': {
-                     'source': sensor.humidity_set
-                               .extra({'date': 'date(timestamp)'})
-                               .values('date')
-                               .annotate(max=Max('value'))
-                               .order_by('-date')[:30],},
-                   'terms': [
-                     {'date_max': 'date'},
-                     'max']}
-                ])
-
-    # Step 2: Create the Chart object
-    cht = Chart(
-            datasource = weatherdata,
-            series_options =
-                [{'options':{
-                    'type': 'line',
-                    'stacking': False},
-                  'terms':{
-                    'date': ['avg'],
-                    'date_min': ['min'],
-                    'date_max': ['max']
-                }}],
-            chart_options =
-                {'title': {
-                    'text': 'Weather Data'},
-                 'xAxis': {
-                    'title': {
-                        'text': 'Date'}}})
-
-    context = {'weatherchart': cht}
     # Step 3: Send the chart object to the template.
     return render(request, 'weather/chart.html', context)
 
